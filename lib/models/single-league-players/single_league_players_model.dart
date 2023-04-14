7e171fe3950405c04ddbdd3ae0a0c67143c3d61d
Daniel Freyr <danielfrs87@gmail.com>
2023-04-14 09:24:36 +0000

    🚧 Single league work in progress

---
+++ lib/models/single-league-players/single_league_players_model.dart
@@ -0,0 +1,18 @@
+import 'package:foosball_mobile_app/models/user/user_response.dart';
+
+class SingleLeaguePlayersModel {
+  List<UserResponse> users;
+  int leagueId;
+
+  SingleLeaguePlayersModel({
+    required this.users,
+    required this.leagueId,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'users': users.map((user) => user.toJson(user)).toList(),
+      'leagueId': leagueId,
+    };
+  }
+}
