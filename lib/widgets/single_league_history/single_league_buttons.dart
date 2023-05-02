eb1a880c448baa8d4aacbb678ccba98ca3f7c895
Daniel Freyr <danielfrs87@gmail.com>
2023-05-02 15:54:57 +0000

    👔 Changed single league history button

--- lib/widgets/single_league_history/single_league_buttons.dart
+++ lib/widgets/single_league_history/single_league_buttons.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:foosball_mobile_app/state/user_state.dart';
 import 'package:foosball_mobile_app/utils/app_color.dart';
+import 'package:foosball_mobile_app/widgets/extended_Text.dart';
 
 class SingleLeagueButtons extends StatelessWidget {
   final UserState userState;
@@ -9,6 +10,10 @@ class SingleLeagueButtons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void close() {
+      Navigator.pop(context);
+    }
+
     return Row(
       children: <Widget>[
         Expanded(
@@ -16,29 +21,19 @@ class SingleLeagueButtons extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.all(4.0),
               child: ElevatedButton(
-                onPressed: () => {},
+                onPressed: () => {close()},
                 style: ElevatedButton.styleFrom(
                     primary: userState.darkmode
                         ? AppColors.lightThemeShadowColor
                         : AppColors.buttonsLightTheme,
                     minimumSize: const Size(100, 50)),
-                child: Text(userState.hardcodedStrings.newMatch),
+                child: ExtendedText(
+                  text: userState.hardcodedStrings.close,
+                  userState: userState,
+                  colorOverride: Colors.white,
+                ),
               ),
-            )),
-        Expanded(
-            flex: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: ElevatedButton(
-                onPressed: () => {},
-                style: ElevatedButton.styleFrom(
-                    primary: userState.darkmode
-                        ? AppColors.lightThemeShadowColor
-                        : AppColors.buttonsLightTheme,
-                    minimumSize: const Size(100, 50)),
-                child: Text(userState.hardcodedStrings.rematch),
-              ),
-            )),
+            ))
       ],
     );
   }
