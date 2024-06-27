bed60902e8f6c53fe2bc81fb4684a8fb4aaed846
Daniel Freyr <danielfrs87@gmail.com>
2024-06-27 22:37:46 +0000

    🐛 Made only one button on double league match details screen

--- lib/widgets/double_league_history/double_league_buttons.dart
+++ lib/widgets/double_league_history/double_league_buttons.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:foosball_mobile_app/state/user_state.dart';
 import 'package:foosball_mobile_app/utils/app_color.dart';
+import 'package:foosball_mobile_app/widgets/dashboard/New_Dashboard.dart';
 import 'package:foosball_mobile_app/widgets/extended_Text.dart';
 
 class DoubleLeagueButtons extends StatelessWidget {
@@ -13,39 +14,33 @@ class DoubleLeagueButtons extends StatelessWidget {
     return Row(
       children: <Widget>[
         Expanded(
-            flex: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: ElevatedButton(
-                onPressed: () => {},
-                style: ElevatedButton.styleFrom(
-                    primary: userState.darkmode
-                        ? AppColors.darkModeButtonColor
-                        : AppColors.buttonsLightTheme,
-                    minimumSize: const Size(100, 50)),
-                child: ExtendedText(
-                    text: userState.hardcodedStrings.newMatch,
-                    userState: userState,
-                    colorOverride: AppColors.white),
+          flex: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: ElevatedButton(
+              onPressed: () => {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => NewDashboard(userState: userState),
+                  ),
+                  (Route<dynamic> route) => false,
+                ),
+              },
+              style: ElevatedButton.styleFrom(
+                primary: userState.darkmode
+                    ? AppColors.darkModeButtonColor
+                    : AppColors.buttonsLightTheme,
+                minimumSize: const Size(100, 50),
               ),
-            )),
-        Expanded(
-            flex: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: ElevatedButton(
-                onPressed: () => {},
-                style: ElevatedButton.styleFrom(
-                    primary: userState.darkmode
-                        ? AppColors.darkModeButtonColor
-                        : AppColors.buttonsLightTheme,
-                    minimumSize: const Size(100, 50)),
-                child: ExtendedText(
-                    text: userState.hardcodedStrings.rematch,
-                    userState: userState,
-                    colorOverride: AppColors.white),
+              child: ExtendedText(
+                text: userState.hardcodedStrings.close,
+                userState: userState,
+                colorOverride: AppColors.white,
               ),
-            )),
+            ),
+          ),
+        ),
       ],
     );
   }
